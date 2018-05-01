7d9fc1db1dcb975d01468cf301d12de6ce8beadc
MrBlue2583 <MrBlue2583@gmail.com>
2018-05-01 15:33:51 +0200

    Changes return values in SubjectResource

--- app/Http/Resources/Subject/SubjectResource.php
+++ app/Http/Resources/Subject/SubjectResource.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Resources\Subject;
 
+use App\Http\Requests\Field\FieldRequest;
+use App\Http\Resources\Degree\DegreeResource;
+use App\Http\Resources\Semester\SemesterResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class SubjectResource extends JsonResource
@@ -14,17 +17,14 @@ class SubjectResource extends JsonResource
      */
     public function toArray($request)
     {
-        $field = $this->field;
-        $semester = $this->semester;
-        $degree = $field->degree;
         return [
             'id' => $this->id,
             'name' => $this->name,
             'type' => $this->type,
             'hours' => $this->hours,
-            'field' => $field->name . '/' . $field->type,
-            'semester' => $semester->type . '/' . $semester->year,
-            'degree' => $degree->name . '/' . $degree->alias,
+            'field' => new FieldResource($this->field),
+            'semester' => new SemesterResource($this->semester),
+            'degree' => new DegreeResource($this->degree),
             'semesterNumber' => $this->semester_number
         ];
     }
